632a552ce16f8e8edb7131e4c363e8f99865b193
Kamalshakoor <kamalshakoor29022000@gmail.com>
2024-06-06 01:05:24 +0500

    added registration,login,logout api's with bcrypt and tested it with postman

---
+++ server/app/controllers/api/v1/auth_controller.rb
@@ -0,0 +1,38 @@
+class Api::V1::AuthController < ApplicationController
+  skip_before_action :verify_authenticity_token
+
+  def register
+    user = User.new(user_params)
+    if user.save
+      session[:user_id] = user.id
+      render json: UserSerializer.new(user).serializable_hash.to_json, status: :created
+    else
+      render json: { errors: user.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  def login
+    user = User.find_by(email: params[:email])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      render json: UserSerializer.new(user).serializable_hash.to_json
+    else
+      render json: { errors: ['Invalid email or password'] }, status: :unauthorized
+    end
+  end
+
+
+  def logout
+    reset_session
+    render json: { message: 'Successfully logged out' }
+  end
+
+
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password,:password_confirmation,:role)
+  end
+  
+end
