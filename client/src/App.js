9e613d17a2531281eb53f040de55952d5d2c6814
Kamalshakoor <kamalshakoor29022000@gmail.com>
2024-06-13 03:42:06 +0500

    added bootstrap , completed auth and also added toaster for notification

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import Home from './components/Home'
+import Signup from './components/auth/Signup'
+import Login from './components/auth/Login'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Routes>
+          <Route exect path="/" element={<Home />}></Route>
+          <Route exect path="/home" element={<Home />}></Route>
+          <Route path='/signup' element={<Signup />}></Route>
+          <Route path='/Login' element={<Login />} ></Route>
+        </Routes>
+      </Router>
+      <ToastContainer />
     </div>
   );
 }
