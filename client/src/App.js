9d1d7447f24c6e4023ba972e4a35eec189249188
Kamalshakoor <kamalshakoor29022000@gmail.com>
2024-06-14 16:40:23 +0500

    added navbar and logout functionality

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,51 @@
 import './App.css';
-import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
-import { ToastContainer } from 'react-toastify';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import Home from './components/Home'
-import Signup from './components/auth/Signup'
-import Login from './components/auth/Login'
+import Home from './components/Home';
+import Signup from './components/auth/Signup';
+import Login from './components/auth/Login';
+import Navbar from './components/Navbar/Navbar';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+import Footer from './components/footer/Footer';
 
 function App() {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  useEffect(() => {
+    if (localStorage.getItem('token')) {
+      setIsAuthenticated(true);
+    } else {
+      setIsAuthenticated(false);
+    }
+  }, []);
+
+  const handleLogout = async (navigate) => {
+    try {
+      console.log('handleLogout called');
+      await axios.delete('http://localhost:3000/api/v1/logout', { withCredentials: true });
+      localStorage.removeItem('token');
+      setIsAuthenticated(false);
+      toast.success('Logout successful');
+      navigate('/login');
+    } catch (error) {
+      console.error('Logout error:', error);
+      toast.error('Logout failed. Please try again.');
+    }
+  };
+
   return (
     <div className="App">
       <Router>
+        <Navbar isAuthenticated={isAuthenticated} handleLogout={handleLogout} />
         <Routes>
-          <Route exect path="/" element={<Home />}></Route>
-          <Route exect path="/home" element={<Home />}></Route>
-          <Route path='/signup' element={<Signup />}></Route>
-          <Route path='/Login' element={<Login />} ></Route>
+          <Route exact path="/" element={<Home />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/login" element={<Login setIsAuthenticated={setIsAuthenticated} />} />
         </Routes>
+        <Footer />
       </Router>
       <ToastContainer />
     </div>
---
+++ client/src/components/footer/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Footer = () => {
+  return <>
+    <footer>
+      <div className="footer bg-light py-2 text-center">
+        <p className="text-muted mt-3">Copyright © 2023. All Rights Reserved.</p>
+      </div>
+    </footer>
+  </>
+}
+
+export default Footer
