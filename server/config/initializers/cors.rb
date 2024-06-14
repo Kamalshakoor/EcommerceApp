1f9c3dc56476747de92693c9442dbe0d116ff082
Kamalshakoor <kamalshakoor29022000@gmail.com>
2024-06-14 16:41:51 +0500

    updated cors setting for delete request

--- server/config/initializers/cors.rb
+++ server/config/initializers/cors.rb
@@ -1,6 +1,9 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins '*', 'http://localhost:3001'
-    resource '*', headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head]
+    origins 'http://localhost:3001' # front-end origin 
+    resource '*',
+      headers: :any,
+      methods: [:get, :post, :put, :patch, :delete, :options, :head],
+      credentials: true # Allow credentials
   end
 end
